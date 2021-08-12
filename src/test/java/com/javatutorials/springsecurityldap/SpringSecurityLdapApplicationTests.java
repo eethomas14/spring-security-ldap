0e21a7237e463cbacdd3e6738de96f7e6ca7625a
eethomas14 <ethomas14@luc.edu>
2021-08-12 08:57:51 -0500

    Initial commit

---
+++ src/test/java/com/javatutorials/springsecurityldap/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatutorials.springsecurityldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
